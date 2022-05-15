adea30505c48dbb353ae4b6c84d220fe943b0969
Zhaolon <33798390+Zhaolon@users.noreply.github.com>
2022-05-15 21:22:01 +0800

    Add files via upload

--- ManifoldLandscape/enumerate.sh
+++ ManifoldLandscape/enumerate.sh
@@ -8,6 +8,6 @@ u=`echo $line | awk -v head="u" -v tail="_b" '{print substr($0, index($0,head)+l
 b=`echo $line | awk -v head="b" -v tail="_c" '{print substr($0, index($0,head)+length(head),index($0,tail)-index($0,head)-length(head))}'`
 c=`echo $line | awk -v head="c" -v tail="_a" '{print substr($0, index($0,head)+length(head),index($0,tail)-index($0,head)-length(head))}'`
 
-l=$[`awk '{if(NF>3) print $0}' stars/u${u}_b${b}_c${c}.star | wc -l`]
+l=$[`grep mrcs stars/u${u}_b${b}_c${c}.star | wc -l`]
 echo $l >> $numberfile
 done < $datafile
